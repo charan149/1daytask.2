0a7eb2a6a7dc45c2c3f2480ed2bef173803335c2
charan149 <charanram149@gmail.com>
2020-05-01 17:56:50 +0530

    create a method

---
+++ src/test/java/org/max/Snap.java
@@ -0,0 +1,20 @@
+package org.max;
+
+public class Snap {
+
+	public void deposit() {
+		System.out.println("2%");
+		}
+public void saving() {
+	System.out.println("6%");
+}
+	public static void main(String[] args) {
+		Snap s=new Snap();
+		s.deposit();
+		s.saving();
+	}
+	
+
+
+	
+}
